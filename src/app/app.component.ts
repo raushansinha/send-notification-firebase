8af079f44c30fb10e2c1fb97916f47f7d68a3a80
Sinha <RLS34420@CHMCA.ORG>
2018-06-21 13:31:49 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http/';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+
+  constructor( private http: HttpClient) { }
+
+  sendNotificationToFirebase(){
+    let body = {
+      "notification":{
+        "title":"New Notification has arrived",
+        "body":"Notification Body",
+        "sound":"default",
+        "click_action":"FCM_PLUGIN_ACTIVITY",
+        "icon":"fcm_push_icon"
+      },
+      "data":{
+        "param1":"value1",
+        "param2":"value2"
+      },
+        "to":"/topics/all",
+        "priority":"high",
+        "restricted_package_name":""
+    }
+    let options = new HttpHeaders().set('Content-Type','application/json');
+    try{
+      this.http.post("https://fcm.googleapis.com/fcm/send",body,{
+        headers: options.set('Authorization', 'key=AAAAy_4ly5Q:APA91bHFTkLAxu3nS3Q1eKWiZoywAiMiJrf_94RjI6xB-_v9l4Nu6RYQ2rny8EgQif48zH4ZSZ_AI_10cadTL9_4JaFQMJFyjX3Fz4oy1HO0oil0_fkb04rlPgbaOk_hbP6iM_c_UM4cEMN9fQ1FgOBxNIqRfyo8HQ'),
+      })
+      .subscribe((res) => {
+       // alert("Result: " + JSON.stringify(res));
+        console.log(res);
+      });
+    }
+    catch(ex) {
+      alert("Error" + ex)
+    }
+  }
 }
